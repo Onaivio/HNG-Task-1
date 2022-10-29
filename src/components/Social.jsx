0d98c496e91267d315683fee11714ed9eb14e555
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-10-29 11:47:40 +0100

    ..

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,8 +4,8 @@ const Social = () => {
   return (
     <div>
       <div className="flex space-x-5 py-7 justify-center">
-        <img src="/slack.svg" alt="" />
-        <img src="/github.svg" alt="" />
+        <a href="https://app.slack.com/client/T042F7V19Q8/composer/draft-29b7b598-dd2d-4b51-8f65-e81163547262/rimeto_profile/U0483H35YCF?cdn_fallback=2" target="_blank"><img src="/slack.svg" alt="" /></a>
+        <a href="https://github.com/Onaivio/HNG-Task-1" target="_blank"> <img src="/github.svg" alt="" /></a>
       </div>
     </div>
   );
